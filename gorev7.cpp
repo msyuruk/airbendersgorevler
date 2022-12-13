bd5700b3f98d42ba7c321382687901e6f1b87772
msyuruk <117199464+msyuruk@users.noreply.github.com>
2022-12-14 00:14:16 +0300

    Update gorev7.cpp

--- gorev7.cpp
+++ gorev7.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
-#include <valarray>
-#include <stdlib.h>
+#include <vector>
 
 int main(int argNo, char* argarray[]) {
-	int katsayi = (int)argarray[0];
-	int satirsayisi = (int)argarray[1];
-	int matrisElemanNo = argNo - 2;
-	std::valarray<int>((matrisElemanNo / satirsayisi) * satirsayisi);
-	std::slice(0, (matrisElemanNo / satirsayisi), satirsayisi);
+	int carpan = (int)argarray[1];
+	int satirsayisi = (int)argarray[2];
+	int arrayindex = 3;
+	int matrisElementNo = (sizeof(argarray)/sizeof(char))-3;
+	int rowVectorElemanNo = (argNo - 3)/satirsayisi;
+	std::vector<int>kucukvektor;
+	std::vector<std::vector<int>>emirhan;
+	for (int i = 0; i < satirsayisi; i++)
+	{
+		for (int j = 0; j < rowVectorElemanNo; j++)
+		{
+			if (i == 0)
+			{
+				kucukvektor.push_back(carpan*(int)argarray[arrayindex]);
+				arrayindex++;
+			}
+			else
+			{
+				kucukvektor.push_back((int)argarray[arrayindex]);
+				arrayindex++;
+			}	
+		}
+		emirhan.push_back(kucukvektor);
+	}
 
+	for (int i = 0; i < satirsayisi; i++)
+	{
+		for(int j=0;j<rowVectorElemanNo;j++)
+		{
+			std::cout << emirhan[i][j];
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "Test" << std::endl;
 	return 0;
 }
