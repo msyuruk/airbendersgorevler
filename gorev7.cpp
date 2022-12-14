56625ce6a87ce643b6b8d1ef6436db684c3d81e0
msyuruk <117199464+msyuruk@users.noreply.github.com>
2022-12-14 21:02:50 +0300

    Update gorev7.cpp

--- gorev7.cpp
+++ gorev7.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 #include <vector>
-
 int main(int argNo, char* argarray[]) {
-	int carpan = (int)argarray[1];
-	int satirsayisi = (int)argarray[2];
+	float katsayi = atof(argarray[1]);
+	int satirsayisi = atoi(argarray[2]);
 	int arrayindex = 3;
 	int rowVectorElemanNo = (argNo - 3)/satirsayisi;
 	std::vector<float>kucukvektor;
 	std::vector<std::vector<float>>emirhan;
-	for (int i = 0; i < argNo-3; i++)
+	for (int i = 0; i < satirsayisi; i++)
 	{
 		for (int j = 0; j < rowVectorElemanNo; j++)
 		{
 			if (i == 0)
 			{
-				kucukvektor.push_back(carpan*(int)argarray[arrayindex]);
+				kucukvektor.push_back(katsayi*atof(argarray[arrayindex]));
 				arrayindex++;
 			}
 			else
 			{
-				kucukvektor.push_back((int)argarray[arrayindex]);
+				kucukvektor.push_back(atof(argarray[arrayindex]));
 				arrayindex++;
 			}	
 		}
 		emirhan.push_back(kucukvektor);
+		kucukvektor = {};
 	}
 
-	for (int i = 0; i < argNo-3; i++)
+	for (int i = 0; i < satirsayisi; i++)
 	{
-		for(int j=0;j<rowVectorElemanNo;j++)
+		for (int j = 0; j < rowVectorElemanNo;j++)
 		{
-			std::cout << emirhan[i][j];
+			std::cout << emirhan[i][j]<< ' ';
 		}
 		std::cout << std::endl;
 	}
